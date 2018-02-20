a850c603681aba42f0c85ab947c0df9e7cacf062
chengmar09 <cwl13khb@gmail.com>
2018-02-20 16:10:00 +0000

    complete unit test for Retrieve class

--- Test_Retrieve.py
+++ Test_Retrieve.py
@@ -1,21 +1,32 @@
-import unittest
+import unittest, csv
 from Retrieve import Retrieve
 
 class Test_Retrieve (unittest.TestCase):
+    address = './data/trial.csv'
     def setUp (self):
-        self.retrieve = Retrieve('./data/trial.csv')
-        self.emptyfile = Retrieve('./data/ti.csv')
-
-    def test_wrongFile(self):
-        data = self.emptyfile.getData("csv")
-        self.assertEqual(data[0], [])
+        self.retrieve = Retrieve(self.address)
+    #     self.emptyfile = Retrieve('./data/ti.csv')
+    #
+    # def test_wrongFile(self):
+    #     data = self.emptyfile.getData("csv")
+    #     self.assertEqual(data[0], [])
 
     def test_header(self):
         data = self.retrieve.getData("csv")
         self.assertEqual(data[0], ['Station name', 'Location', 'Frequency', 'lat', 'long'])
 
-    def read_data_point(self):
+    def test_data_point(self):
+        data = self.retrieve.getData("csv")
+        print(data[3])
+        self.assertEqual(data[3][1], 'Waterloo')
 
+    def test_Rows_No(self):
+        data = self.retrieve.getData("csv")
+        with open(self.address, "r") as f:
+            reader = csv.reader(f, delimiter = ',')
+            readerData = list(reader)
+            row_count = len(readerData)
+        self.assertEqual(self.retrieve.getCounter(), row_count)
 
 if __name__ == "__main__":
     unittest.main()
