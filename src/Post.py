6e20ce583976492d65e38c1537a9dcc07ec7bebc
AdityaNPL <adityaniraula@gmail.com>
2018-03-16 20:47:43 +0000

    New venv

--- src/Post.py
+++ src/Post.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python
 import psycopg2
 import sys
+import pandas as pd
+import hdfs
+
+import os
+
 
 def main():
-	#Define our connection string
-	conn_string = "host='localhost' dbname='testDataServer' user='adity' password='AdityaD1998'"
-
-	# print the connection string we will use to connect
-	print ("Connecting to database")
-
-
-	# get a connection, if a connect cannot be made an exception will be raised here
-	conn = psycopg2.connect(conn_string)
-
-	# conn.cursor will return a cursor object, you can use this cursor to perform queries
-	cursor = conn.cursor()
-	print ("Connected!")
+    try:
+        client = hdfs.client.Client("hdfs://udltest3.cs.ucl.ac.uk:8020")
+        print(client)
+        print(client.content('/'))
+    except Exception as e:
+        print("ERROR")
+        print(e)
 
 if __name__ == "__main__":
-	main()
+    main()
