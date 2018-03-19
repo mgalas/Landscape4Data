6c8bc930b21622b3b0cedd59410a7cf0be8ff365
AdityaNPL <adityaniraula@gmail.com>
2018-03-19 20:55:18 +0000

    Oozie scheduler

--- src/Post.py
+++ src/Post.py
@@ -1,17 +1,15 @@
-#!/usr/bin/python
-import psycopg2
+
 import sys
 import pandas as pd
 import hdfs
-
 import os
 
 
 def main():
     try:
-        client = hdfs.client.Client("hdfs://udltest3.cs.ucl.ac.uk:8020")
-        print(client)
-        print(client.content('/'))
+        client =hdfs.InsecureClient('http://udltest1.cs.ucl.ac.uk:50075')
+        status = client.status('dat/features')
+        print(status)
     except Exception as e:
         print("ERROR")
         print(e)
